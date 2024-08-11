b993ece1272b7cc0aaa897ba873f4a8c48dfa8bf
Haru-forPotepanExam <ha.stu24.ru@gmail.com>
2024-08-11 20:10:50 +0900

    add: 基本的なCategoryモデルの設定、初期データの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create!(
+  [
+    {name: "野菜"},
+    {name: "穀物（米・シリアル等）"},
+    {name: "水産物"},
+    {name: "畜産物(肉)"},
+    {name: "・乳製品・大豆製品"},
+    {name: "果物"},
+    {name: "粉類"},
+    {name: "その他"}
+  ]
+)
